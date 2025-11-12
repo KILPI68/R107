898a0fd0222e0b6b44f60610ce6db1ed0bef833d
KILPI68 <kilpi09102008@gmail.com>
2025-11-12 16:34:22 +0100

    Delete exo5.py

--- exo5.py
+++
@@ -1,9 +0,0 @@
-# exo5.py
-
-jour = int(input("Entrez le jour : "))
-heure = int(input("Entrez l'heure : "))
-minute = int(input("Entrez les minutes  : "))
-
-duree = (jour - 1) * 24 * 60 + heure * 60 + minute
-
-print(f"Depuis le début du mois, il s'est écoulé {duree} minutes.")
