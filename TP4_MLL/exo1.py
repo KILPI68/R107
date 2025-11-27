99e6c3386be77f7ca52d8339e29e595ff3f4d6a8
KILPI68 <kilpi09102008@gmail.com>
2025-11-27 08:56:35 +0100

    TP4

--- TP4_MLL/exo1.py
+++ TP4_MLL/exo1.py
@@ -1,8 +1,10 @@
-x=float(input("Entrez votre chiffre:"))
-n = 0
-list=[]
+nombre = float(input("Vous cherchez la table de multiplication de quel nombre ? "))
+resultats = []
 
+for i in range(10):
+    resultat = nombre * i
+    resultat_arrondi = round(resultat, 1)
+    resultats.append(resultat_arrondi)
 
-for i in range(10) :
-    print(f"{x} * {n+1} = {round(x * n+1,2)}")
-    n=n+1
+for i in range(10):
+    print(f"{nombre:.1f}*{i}={resultats[i]:.1f}")
